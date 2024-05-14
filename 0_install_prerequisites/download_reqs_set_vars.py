daa215ec9933087320e108a8b5116ebdd32a7c87
Oliver Zarate <41704658+odog96@users.noreply.github.com>
2024-05-14 17:14:07 -0600

    Update download_reqs_set_vars.py
    udpated pip install

--- 0_install_prerequisites/download_reqs_set_vars.py
+++ 0_install_prerequisites/download_reqs_set_vars.py
@@ -1,1 +1,3 @@
+# !pip install --no-cache-dir -r 0_install_prerequisites/requirements.txt
 !pip install --no-cache-dir -r 0_install_prerequisites/requirements.txt
+!pip install pinecone-client==3.2.2 --upgrade
